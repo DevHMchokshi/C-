36c70af6d15b473b520421cef039b2bbec2a71e7
dev <devchokshi100@gmail.com>
2025-05-23 11:59:34 +0530

    added

---
+++ string2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    // string s;
+    // cout<<"enter string";
+    // getline(cin,s);
+    // cout<<c;
+
+    char c[100];
+    cout<<"enter string";
+    cin.get(c,100);
+    cout<<c;
+
+    return 0;
+}
