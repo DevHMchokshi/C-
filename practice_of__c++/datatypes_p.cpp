0d606c870e3f02ec9bdd637ee8e5c679950d5e62
dev <devchokshi100@gmail.com>
2025-05-25 11:22:05 +0530

    Added new files

---
+++ practice_of__c++/datatypes_p.cpp
@@ -0,0 +1,14 @@
+// print all datatypes in c
+#include<iostream>
+// identify var function ,and datatypes
+using namespace std;
+int main(){
+    int i=12;    
+    double d=12.12;
+    char c='a';
+    char c1[10]="test";
+    cout<<"int i="<<  i  <<  endl  <<"double :" <<  d  << endl <<"character :" <<  c  << endl  <<"string is ="<<c1;
+    
+return 0;
+}
+
---
+++ practice_of__c++/userinput_p.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int i;
+    double d;
+    char c;
+    char str[10];
+    cout<<"enter string";
+    cin>>i;
+    cout<<"enter double val";
+    cin>>d;
+    cout<<"enter char";
+    cin>>c;
+    cout<<"enter string";
+    cin>>str;
+    cout<<i<<endl<<d<<endl<<c<<endl<<str;
+    return 0;
+}
